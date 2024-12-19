c85f53e04233d747479055fc16e3cad879812b4e
Shivli <dimri116sd@gmail.com>
2024-12-19 09:18:54 +0530

    added

---
+++ mad_libs_game.py
@@ -0,0 +1,11 @@
+#you cant take any bunch of words as input and it converts them into a story very randomely
+#let users input fill ups like adjectives, nouns, pronouns ets
+#like roses are {let user input color}, {let user input flower name} are blue, I love you!
+
+print("enter a colour : ")
+color = input()
+print("enter a flower's name : ")
+flower_name = input()
+print("roses are", color)
+print(flower_name, "are blue")
+print("i love you!")
